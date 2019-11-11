ff831ba25062292c1cd68348965f4eed02eaa7fe
Adam ? <adamznow@gmail.com>
2019-11-11 00:36:16 -0800

    Begin framework for mod contexts, hopefully for cool mod installation

---
+++ libmodloader/src/modloader.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdlib.h>
+
+// ModContext
+class ModContext
+{
+    public:
+        ModContext(std::string path_, void* handle_) : path(path_), handle(handle_) {}
+        std::string path;
+        void* handle;
+}
